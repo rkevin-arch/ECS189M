8a823262077748a010a72a331160289dc268fb77
rkevin <rk@rkevin.dev>
2019-12-07 19:46:19 -0800

    add tinyuaf challenge

---
+++ challenges/binary/tinyuaf/tinyuaf.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isWinner = 0;
+struct Lame {
+    int x;
+} Lame;
+
+struct Uaf
+{
+    int * x;
+} Uaf;
+
+int main() {
+   setbuf(stdin,NULL);
+   setbuf(stdout,NULL);
+   struct Uaf * currentStruct = malloc(sizeof(struct Uaf));
+   free(currentStruct);
+   struct Lame * lameStruct = malloc(sizeof(struct Lame));
+   while(!isWinner) {
+      printf("You aren't an admin. What would you like to do?\n");
+      printf("Admin variable address: %p\n", (void *) &isWinner);
+      printf("Admin: %d\n", isWinner);
+      int choice = 0;
+      scanf("%d", &choice);
+      switch(choice) {
+         case 0:
+            scanf("%d", &(lameStruct->x));
+            break;
+         case 1:
+            *(currentStruct->x) = 1;
+            break;
+         default:
+            break;
+      }
+   }
+   printf("Welcome, admin!\n");
+   system("/bin/sh");
+}
+
