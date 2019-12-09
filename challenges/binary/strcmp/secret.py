c622dc0d8b869f9939c3c74814915f953dc716c8
Uttie Gunbula <uttie@twinpeaks.cs.ucdavis.edu>
2019-12-08 19:17:16 -0800

    add strcmp to test

---
+++ challenges/binary/strcmp/secret.py
@@ -0,0 +1,6 @@
+import random
+flag="CSCaUCD{81N4RY_534RCH_50AF9F479667C26EBF4EE74E956371D5}"
+def genpasswd(l):
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    return "".join([random.choice(alphabet) for _ in range(l)])
+password=genpasswd(20)
