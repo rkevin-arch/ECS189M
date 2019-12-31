500ad0b99d6b35118eb093859d606f65831d1bae
rkevin <rk@rkevin.dev>
2019-12-31 06:40:29 -0800

    ported over binaryaddition challenge

---
+++ challenges/linux_misc/binaryaddition/dist/server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+from timeout_decorator import *
+from subprocess import call
+import random
+import sys
+import os
+import struct
+
+@timeout(10)
+def questions():
+    for i in range(50):
+        a=random.randint(1,2147483647)
+        b=random.randint(1,2147483647)
+        sys.stdout.write("Question %d : "%i)
+        sys.stdout.write(struct.pack("I",a))
+        sys.stdout.write(" + ")
+        sys.stdout.write(struct.pack("I",b))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        c=sys.stdin.read(4)
+        c=int(struct.unpack("I",c)[0])
+        if(c!=a+b):
+            print "Wrong! Go back to grade school and learn your addition!"
+            sys.stdout.flush()
+            sys.exit()
+
+os.chdir("/home/hal")
+
+print "John just noticed his homework is due in 10 seconds!"
+print "Help him finish his homework, which contains 50 additions."
+print "However, his teacher is a computer! Hal is giving his homework in binary."
+print "You have to answer the question in binary too."
+print 'The format is "Question x : num + num", where num is a 4 byte little endian integer.'
+print "Your answer should be another 4 byte little endian integer, with no newlines at the end."
+print "Ready? Go!"
+try:
+    questions()
+    
+    print "You did it! John would like to reward you with a shell."
+    print "Find the flag there!"
+    sys.stdout.flush()
+    call("/bin/bash")
+except TimeoutError:
+    print "You took too much time! The homework was collected and John is now a very sad kid."
+
