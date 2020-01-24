e8512eabb311dc77aacdf457dbcf719857bdedad
rkevin <rk@rkevin.dev>
2020-01-24 03:51:21 -0800

    we might as well just ditch the localhost stuff to make stuff easier

--- challenges/webapp/csrf/load_page.js
+++ challenges/webapp/csrf/load_page.js
@@ -3,9 +3,6 @@ var system = require('system');
 var webPage = require('webpage');
 var TIMER_EXIT = 5000;
 
-var HOST = "localhost";
-var PORT = "8080";
-
 // Command line arguments
 if (system.args.length < 2){
   console.log('Usage: ' + system.args[0] +  ' <Cookie>');
@@ -13,37 +10,10 @@ if (system.args.length < 2){
 } 
 
 var page = webPage.create();
-var url = 'http://' + HOST + ":" + PORT + "/posts";
+var url = 'https://csrf.webchal.twinpeaks.cs.ucdavis.edu/posts';
 var cookie = system.args[1];
 
 console.log("parsed cookie: " + cookie);
-
-phantom.addCookie({
-  name: "webchal_csrf_forum_sessid",
-  value: cookie,
-  domain: "localhost",
-  secure: false,
-  httponly: true,
-  path: "/"
-});
-
-phantom.addCookie({
-  name: "beamsplitter_csrf",
-  value: system.env["beamsplitter_cookie"],
-  domain: "localhost",
-  secure: false,
-  httponly: true,
-  path: "/"
-});
-
-phantom.addCookie({
-  name: "message",
-  value: "Nice_try!_But_this_isn't_a_XSS_challenge_so_try_a_CSRF_attack_instead!",
-  domain: "localhost",
-  secure: false,
-  httponly: false,
-  path: "/"
-});
 
 phantom.addCookie({
   name: "webchal_csrf_forum_sessid",
