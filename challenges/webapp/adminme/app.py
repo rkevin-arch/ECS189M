566a8703ade1280d82352f9ca162f0bb77b45e7e
rkevin <rk@rkevin.dev>
2020-01-23 16:29:39 -0800

    cosmetic changes, it kinda sucks but it works

--- challenges/webapp/adminme/app.py
+++ challenges/webapp/adminme/app.py
@@ -8,16 +8,27 @@ TEMPLATE="""<!DOCTYPE html>
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:400,300,600">
         <link rel="stylesheet" href="https://skeleton-framework.github.io/css/normalize.css">
         <link rel="stylesheet" href="https://skeleton-framework.github.io/css/skeleton.css">
+        <style>
+            .border {
+              padding: 2.5rem 3rem;
+              border: 1px solid #E1E1E1;
+              border-radius: 4px;
+            }
+        </style>
     </head>
-    <body>
-        <h1>Flag dispenser</h1>
-        <p>Welcome to the flag dispenser!</p>
-        <p>%s</p>
+    <body class="container">
+        <div class="six columns offset-by-three border boxshadow">
+        <div class="row">
+            <h3>Flag dispenser</h3>
+        </div>
+        <div class="row">Welcome to the flag dispenser!</div>
+        <div class="row" style="overflow-wrap: break-word;">%s</div>
+        </div>
     </body>
 </html>
 """
 NOFLAG=TEMPLATE%"Unfortunately, you must be an admin to get the flag. Login is currently closed."
-FLAG=TEMPLATE%"Welcome back, admin! Here's your flag: <code>ECS{15_4DM1N_TRU3_9C8861B05695569D3F67382254CD170B}</code>"
+FLAG=TEMPLATE%"Welcome back, admin! Here's your flag: ECS{15_4DM1N_TRU3_9C8861B05695569D3F67382254CD170B}"
 
 @route('/')
 def index():
