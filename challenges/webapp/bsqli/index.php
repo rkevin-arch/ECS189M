758170ee61b6744cf9a2a706984436b8c74ecf70
rkevin <rk@rkevin.dev>
2020-01-25 19:17:47 -0800

    change bsqli title

--- challenges/webapp/bsqli/index.php
+++ challenges/webapp/bsqli/index.php
@@ -18,7 +18,7 @@ if(isset($_REQUEST["username"])){
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>ECS189M Web Challenge: sqli</title>
+        <title>ECS189M Web Challenge: bsqli</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:400,300,600">
         <link rel="stylesheet" href="https://skeleton-framework.github.io/css/normalize.css">
