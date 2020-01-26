07f7ee93f3234405d580a792b9f623a982097998
aenygma <aenygma@protonmail.com>
2020-01-25 22:38:10 -0800

    add timeout for phantom to be killed

--- challenges/webapp/jsprog/app.py
+++ challenges/webapp/jsprog/app.py
@@ -1,13 +1,15 @@
 import re
 import os
+import secrets
 import binascii
 import subprocess
-import secrets
 
 from collections import namedtuple
 from bottle import get, post, request, response
 from bottle import run, static_file, redirect, template
 
+TIMEOUT = "/usr/bin/timeout"
+TIMEOUT_TIME = 2
 PHANTOMJS = "/usr/bin/phantomjs"
 JS_FILE = "/home/web/load_page.js"
 ENABLE_PHANTOMJS = True # should always be true unless testing webapp
@@ -24,16 +26,23 @@ def answer():
         p="You didn't submit any code!"
 
     if ENABLE_PHANTOMJS:
-        p=subprocess.run([PHANTOMJS, JS_FILE, js], stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-        p=p.stdout.decode()
+        process = subprocess.run([TIMEOUT, TIMEOUT_TIME, PHANTOMJS, JS_FILE, js],
+                    stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        p = process.stdout.decode()
+        if process.returncode != 0:
+            p += "\nWARNING: Your code has timed out! Check for infinite loops "\
+                    "or other nasty stuff in your code!"
     else:
-        p="You should never see this. Contact Kevin because he forgot to remove testing code."
+        p="You should never see this. Contact Kevin because he forgot to remove "\
+            "testing code."
 
     return template('templates/index.tpl', {"code": js, "msg": p.split('\n')})
 
 @get('/reflect')
 def reflectdummypage():
-    if request.environ.get('REMOTE_ADDR')=='127.0.0.1': #this works behind beamsplitter cuz docker doesn't use localhost when forwarding requests
+    if request.environ.get('REMOTE_ADDR')=='127.0.0.1':
+    # this works behind beamsplitter cuz docker doesn't use
+    # localhost when forwarding requests
         return template('templates/reflect.tpl')
     else:
         return template('templates/cheat.tpl')
