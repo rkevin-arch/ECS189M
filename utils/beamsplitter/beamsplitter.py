e228410fab3d15dc924776c92e3afe89160a0398
rkevin <rk@rkevin.dev>
2020-01-25 21:05:53 -0800

    finalize challenges, enter production

--- utils/beamsplitter/beamsplitter.py
+++ utils/beamsplitter/beamsplitter.py
@@ -12,13 +12,13 @@ validservices=[
     "adminme",
     "phpeval",
     "jsprog",
-    #"xss",
+    "xss",
     "csrf",
     "sqli",
     "searchbar",
     "bsqli",
-    #"babylfi",
-    #"lfirce",
+    "babylfi",
+    "lfirce",
     #"finalwebapp",
 ]
 whitelist=[
@@ -35,7 +35,7 @@ whitelist=[
     "73.66.52.69",
     "71.197.105.3"
 ]
-production=False
+production=True
 services={}
 MAX_SERVICES=100
 ERROR="http://localhost:8080/webchal/error.html?"
@@ -79,7 +79,7 @@ class Service:
     def __init__(self,imgname,port,cookie):
         self.name=imgname
         self.port=port
-        self.container=docker.containers.run(imgname, remove=True, detach=True, tty=not production, mem_limit="64m", memswap_limit="128m", nano_cpus=25*(10**7), ports={'8080/tcp':('127.0.0.1',port)}, environment={"beamsplitter_cookie": cookie})
+        self.container=docker.containers.run(imgname, remove=True, detach=True, tty=True, mem_limit="64m", memswap_limit="128m", nano_cpus=25*(10**7), ports={'8080/tcp':('127.0.0.1',port)}, environment={"beamsplitter_cookie": cookie})
         self.creationtime=datetime.datetime.now()
         self.lastaccesstime=datetime.datetime.now()
         self.alive=True
