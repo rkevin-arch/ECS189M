c6542a1be401693f372c349e603a1d077f9fcf82
rkevin <rk@rkevin.dev>
2020-01-25 19:19:52 -0800

    add two lfi challenges and a huge load of lines

---
+++ challenges/webapp/babylfi/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ECS189M Web Challenge: babylfi</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:400,300,600">
+        <link rel="stylesheet" href="https://skeleton-framework.github.io/css/normalize.css">
+        <link rel="stylesheet" href="https://skeleton-framework.github.io/css/skeleton.css">
+        <style>
+            .border {
+              padding: 2.5rem 3rem;
+              border: 1px solid #E1E1E1;
+              border-radius: 4px;
+            }
+        </style>
+    </head>
+    <body class="container">
+        <div class="twelve columns border boxshadow">
+            <div class="row">
+                <h3>Shakespearean Sonnets</h3>
+            </div>
+            <div class="row">
+                <p>Feel free to enter a number between 1 and 154 to view a Shakespear sonnet.</p>
+            </div>
+            <form>
+                <input type="text" placeholder="file" name="file"><br>
+                <button type="submit">Read</button><br>
+            </form>
+            <div class="row"><?php if(isset($_REQUEST["file"])) echo file_get_contents("/var/www/sonnets/".$_REQUEST["file"]);?></div>
+        </div>
+    </body>
+</html>
---
+++ challenges/webapp/lfirce/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ECS189M Web Challenge: lfirce</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:400,300,600">
+        <link rel="stylesheet" href="https://skeleton-framework.github.io/css/normalize.css">
+        <link rel="stylesheet" href="https://skeleton-framework.github.io/css/skeleton.css">
+        <style>
+            .border {
+              padding: 2.5rem 3rem;
+              border: 1px solid #E1E1E1;
+              border-radius: 4px;
+            }
+        </style>
+    </head>
+    <body class="container">
+        <div class="twelve columns border boxshadow">
+            <div class="row">
+                <h3>Elizabeth Barrett Browning Sonnets</h3>
+            </div>
+            <div class="row">
+                <p>Feel free to enter a number between 1 and 44 to view an Elizabeth Barrett Browning sonnet.</p>
+            </div>
+            <form>
+                <input type="text" placeholder="file" name="file"><br>
+                <button type="submit">Read</button><br>
+            </form>
+            <div class="row"><?php if(isset($_REQUEST["file"])) include("/var/www/sonnets/".$_REQUEST["file"]);?></div>
+        </div>
+    </body>
+</html>
