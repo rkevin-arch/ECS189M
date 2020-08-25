cbf86ec3d449d3887c4265aa68a91c01491816d3
rkevin <rk@rkevin.dev>
2020-08-25 14:27:18 -0700

    revamp xinetd_base to hopefully reduce size, also add helper script to build xinetd_base

---
+++ utils/xinetd/build-xinetd-base.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -xe
+
+UTILS="cat bash base64 whoami uname echo more hostname touch pwd true false ls sha1sum id md5sum sha256sum sha512sum"
+CONTAINER=`docker run --rm -d --entrypoint /bin/sleep xinetd:latest 10000`
+
+# copy a binary and its library files
+copybin() {
+    u=$1
+    if docker exec $CONTAINER [ -f /bin/$u ]; then
+        docker cp $CONTAINER:/bin/$u $TARGET/bin/
+    else
+        docker cp $CONTAINER:/usr/bin/$u $TARGET/bin/
+    fi
+    for l in `docker exec $CONTAINER sh -c 'ldd $(which '$u')'`; do
+        if echo $l | grep '^/lib' >/dev/null; then
+            mkdir -p $TARGET$(dirname $l)
+            docker cp -L $CONTAINER:$l $TARGET$l
+        fi
+    done
+}
+
+# binary environment
+TARGET=../../xinetd_base/binary
+rm -rf $TARGET
+mkdir $TARGET
+
+mkdir $TARGET/bin
+for u in cat bash base64 whoami uname echo more hostname touch pwd true false ls sha1sum id md5sum sha256sum sha512sum; do
+    copybin $u
+done
+cp ../noaslr/noaslr $TARGET/bin/
+
+docker cp $CONTAINER:/lib/terminfo $TARGET/lib/terminfo
+
+mkdir $TARGET/etc
+
+cat <<EOF >$TARGET/etc/group
+root:x:0:
+user:x:1337:
+admin:x:1338:
+EOF
+
+cat <<EOF >$TARGET/etc/passwd
+root:x:0:0:root:/:/bin/bash
+user:x:1337:1337::/:/bin/bash
+admin:x:1338:1338::/:/bin/bash
+EOF
+
+# python environment
+TARGET=../../xinetd_base/python
+rm -rf $TARGET
+cp -lr ../../xinetd_base/binary $TARGET
+
+copybin python3
+mkdir -p $TARGET/usr/bin/python3.7
+docker cp $CONTAINER:/usr/bin/python3.7 $TARGET/usr/bin/python3.7
+mkdir $TARGET/usr/bin/python3.7/dist-packages/
+docker cp $CONTAINER:/usr/lib/python3/dist-packages/cryptography $TARGET/usr/bin/python3.7/dist-packages/
+docker cp $CONTAINER:/usr/local/lib/python3.7/dist-packages/timeout_decorator $TARGET/usr/bin/python3.7/dist-packages/
+
+docker stop $CONTAINER
